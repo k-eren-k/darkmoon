04707177c0344b246dd31bf68dcc735861ec515e
Eren <114621741+k-eren-k@users.noreply.github.com>
2024-05-04 20:05:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const axios = require('axios');
+const axios = require('axios');const ejs = require('ejs');
+const path = require('path');
 const app = express();
 app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.render('index');
  });
